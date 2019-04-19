22be9597d678eabd6f7849a3336e1ee8e5edaf3c
Jeroen P. Broks <facebook@tbbs.nl>
2019-04-19 15:45:38 +0200

    vUpdate

--- GJCR6.cs
+++ GJCR6.cs
@@ -1,28 +1,30 @@
 // Lic:
-// 	GJCR6 for .NET
-// 	
-// 	
-// 	
-// 	
-// 	(c) Jeroen P. Broks, 2018, All rights reserved
-// 	
-// 		This program is free software: you can redistribute it and/or modify
-// 		it under the terms of the GNU General Public License as published by
-// 		the Free Software Foundation, either version 3 of the License, or
-// 		(at your option) any later version.
-// 		
-// 		This program is distributed in the hope that it will be useful,
-// 		but WITHOUT ANY WARRANTY; without even the implied warranty of
-// 		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// 		GNU General Public License for more details.
-// 		You should have received a copy of the GNU General Public License
-// 		along with this program.  If not, see <http://www.gnu.org/licenses/>.
-// 		
-// 	Exceptions to the standard GNU license are available with Jeroen's written permission given prior 
-// 	to the project the exceptions are needed for.
-// Version: 18.09.30
+// GJCR6 in C#
+// Graphic Interface for JCR6 in C#
+// 
+// 
+// 
+// (c) Jeroen P. Broks, 
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// 
+// Please note that some references to data like pictures or audio, do not automatically
+// fall under this licenses. Mostly this is noted in the respective files.
+// 
+// Version: 19.04.19
 // EndLic
-﻿using System;
+
+using System;
 using System.Text;
 using System.Collections.Generic;
 using Gtk;
@@ -364,7 +366,7 @@ namespace GJCR
         public static void Init(string[] args) // Args are copied in order to allow direct JCR6 loading from the command line
         {
             MKL.Lic    ("GJCR6 for .NET - GJCR6.cs","GNU General Public License 3");
-            MKL.Version("GJCR6 for .NET - GJCR6.cs","18.09.30");
+            MKL.Version("GJCR6 for .NET - GJCR6.cs","19.04.19");
             if (System.IO.File.Exists(configfile))
             {
                 Console.WriteLine($"Loading: {configfile}");
@@ -490,3 +492,4 @@ namespace GJCR
 
     }
 }
+
